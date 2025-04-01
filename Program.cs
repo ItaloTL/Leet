0c7a73edc561f51ceb2243e695fa480e5cc86b07
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-01 19:38:26 -0300

    tarefa leet concluida

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Tarefa "Leet"
+Console.Clear();
+Console.WriteLine("O que deseja traduzir?: ");
+Console.WriteLine(
+    Console.ReadLine()!
+    .ToUpper()
+        .Replace("A", "4")
+        .Replace("E", "3")
+        .Replace("I", "1")
+        .Replace("L", "1")
+        .Replace("O", "0")
+        .Replace("T", "7")
+        .Replace("S", "5"));
